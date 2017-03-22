24ba76248bd24d925034eda4ab140d5771d5f7a4
moriel5 <moriel5@users.noreply.github.com>
2017-03-22 10:02:22 +0200

    1.0.1
    Added symlink creation, to ease launching Chromium from the terminal.

--- Download Chromium.sh
+++ Download Chromium.sh
@@ -2,5 +2,6 @@
 wget https://download-chromium.appspot.com/dl/Linux_x64?type=***** -O chrome-linux.zip
 unzip chrome-linux.zip
 mv -v -f chrome-linux /opt
+ln /opt/chrome-linux/chrome /usr/bin/chromium
 rm chrome-linux.zip
 exit
