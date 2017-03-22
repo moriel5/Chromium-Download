1bb8243fe4d4ac4e8669b3cd4c1ac45c421cef0e
moriel5 <moriel5@users.noreply.github.com>
2017-03-22 09:52:34 +0200

    Update Download Chromium.sh

--- Download Chromium.sh
+++ Download Chromium.sh
@@ -1,5 +1,4 @@
-# Replace ***** with type of build you wish to download.
-wget https://download-chromium.appspot.com/dl/Linux_x64?type=***** -O chrome-linux.zip
+wget https://download-chromium.appspot.com/dl/Linux_x64?type=snapshots -O chrome-linux.zip
 unzip chrome-linux.zip
 mv -v -f chrome-linux /opt
 rm chrome-linux.zip
